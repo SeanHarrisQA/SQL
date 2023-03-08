db807209e5c10dc1d84f41e9d745bd504b19b891
SeanHarrisQA <126670640+SeanHarrisQA@users.noreply.github.com>
2023-03-08 14:21:06 +0000

    Update movielens_answers.sql

--- movielens_answers.sql
+++ movielens_answers.sql
@@ -9,7 +9,6 @@ SELECT `title`, `release_date` FROM `movies`
   ORDER BY `release_date`;
   
 -- Question 2: Without using LIMIT, list the titles of the movies with the lowest average rating.
--- Semi-complete, used a limit
 SELECT `title`, `id` FROM `movies`
   WHERE `id` IN (
     -- Gives field containg the movie_id for each film with the lowest average rating
@@ -17,39 +16,13 @@ SELECT `title`, `id` FROM `movies`
       GROUP BY `movie_id`
       HAVING AVG(`rating`) = (
 	    -- Calculate the lowest average rating
-        SELECT AVG(`rating`) AS lowest_average_rating FROM `ratings`
-          GROUP BY `movie_id`
-          ORDER BY AVG(`rating`) ASC LIMIT 1
+        SELECT MIN(`mean_rating`.`average_rating`) FROM (
+            SELECT AVG(`rating`) AS `average_rating` FROM `ratings`
+              GROUP BY `movie_id`
+		  ) AS `mean_rating`
     )
     ORDER BY `movie_id` ASC
   );
-  
-  
-  -- Gives average rating for each movie
-  SELECT AVG(`rating`) AS average_rating FROM `ratings`
-          GROUP BY `movie_id`;
-          
-SELECT MIN() FROM (
-    SELECT AVG(`rating`) AS average_rating FROM `ratings`
-      GROUP BY `movie_id`
-  );
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 -- Question 3: List the unique records for Sci-Fi movies where male 24-year-old students have given 
 -- 5-star ratings
